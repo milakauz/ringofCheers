8c997ced8caf7fe122250f958bfbe34da060da6a
Selina Karlin <karlinselina@gmail.com>
2023-11-03 09:28:24 +0100

    new model for game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  
+  currentCard: string = '';
+  game!: Game;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game);
+
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
   takeCard() {
-    this.pickCardAnimation = true;
+    //only clicking on stack and picking card if boolean is false. It's only possible every 1.5s.
+    if (!this.pickCardAnimation) {
+      // last value from array will be returned and deleted
+      this.currentCard = this.game.stack.pop()!;
+      this.pickCardAnimation = true;
+      
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+        console.log('new card:' + this.currentCard);
+        console.log(this.game);
+      }, 1000);
+    }
   }
 
 }
