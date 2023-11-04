52b0d522aaf83bff141b9bb89c82e52abd47acee
Selina Karlin <karlinselina@gmail.com>
2023-11-04 15:53:36 +0100

    adding player, game info and button (addPlayer) styling those feat: openDialog() setting currentPlayer and styling for current player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog } from "@angular/material/dialog";
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
@@ -10,14 +12,14 @@ import { Game } from 'src/models/game';
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string = '';
+  name!: string;
   game!: Game;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
+
 
   ngOnInit(): void {
     this.newGame();
-    console.log(this.game);
-
   }
 
   newGame() {
@@ -30,14 +32,25 @@ export class GameComponent implements OnInit {
       // last value from array will be returned and deleted
       this.currentCard = this.game.stack.pop()!;
       this.pickCardAnimation = true;
-      
+      this.game.currentPlayer++;
+      // current Player should start from beginning after reaching last player. 
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
         this.pickCardAnimation = false;
-        console.log('new card:' + this.currentCard);
-        console.log(this.game);
       }, 1000);
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
+    });
   }
 
 }
