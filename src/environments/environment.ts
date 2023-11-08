ddaecd030f29c331e7731c40d3954108a74e4069
Selina Karlin <karlinselina@gmail.com>
2023-11-08 19:38:23 +0100

    adding environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAT6Li6yGyMrDB5ezjFPY6W7ZaSIQSIhKI",
+        authDomain: "ring-of-cheers.firebaseapp.com",
+        projectId: "ring-of-cheers",
+        storageBucket: "ring-of-cheers.appspot.com",
+        messagingSenderId: "683810670313",
+        appId: "1:683810670313:web:7ea4599ebe33e88a045278"
+    }
+}
